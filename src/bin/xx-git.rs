2aea9dec8cb49af5c7998442ab6dc03ad370ac77
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-04-30 14:42:33 -0400

    src/bin/xx-git.rs:parse relays_public

--- src/bin/xx-git.rs
+++ src/bin/xx-git.rs
@@ -1,11 +1,51 @@
-use gnostr_bins::get_weeble;
-use gnostr_bins::get_blockheight;
-use gnostr_bins::get_wobble;
-fn main() {
+//use gnostr_bins::get_blockheight;
+//use gnostr_bins::get_weeble;
+//use gnostr_bins::get_wobble;
 
-println!("{:}/{:}/{:}",gnostr_bins::get_weeble().unwrap(),gnostr_bins::get_blockheight().unwrap(),gnostr_bins::get_wobble().unwrap());
+fn parse(input: &str) -> Option<Vec<String>> {
+    let mut part = String::new();
+    let mut collected = Vec::new();
+
+    let mut char_iter = input.chars();
+
+    if char_iter.next() != Some('[') {
+        return None;
+    }
+
+    loop {
+        match char_iter.next()? {
+            ']' => {
+                collected.push(part);
+                return Some(collected);
+            }
+            ',' | ' ' => {
+                if !part.is_empty() {
+                    collected.push(part);
+                    part = String::new();
+                }
+            }
+            x => part.push(x),
+        }
+    }
+}
+fn main() {
+    let weeble = gnostr_bins::get_weeble();
+    let wobble = gnostr_bins::get_wobble();
+    let blockheight = gnostr_bins::get_blockheight();
+
+    println!(
+        "{:}/{:}/{:}",
+        weeble.unwrap(),
+        blockheight.unwrap(),
+        wobble.unwrap()
+    );
+
+    let relays_public = gnostr_bins::get_relays_public();
+    println!("{:}", relays_public.clone().unwrap());
     //let version = xx::git::get_git_version();
     //println!("{}", version.unwrap());
+    println!("{:?}", parse(&relays_public.as_ref().unwrap()));
+
     let git = xx::git::Git::new(".".into());
 
     if git.is_repo() {
