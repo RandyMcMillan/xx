e956369111cae4a476b58bb0c5d90e0ae92991e7
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-04-30 09:13:19 -0400

    src/bin/xx-git.rs:print current_branch

--- src/bin/xx-git.rs
+++ src/bin/xx-git.rs
@@ -1,10 +1,22 @@
 fn main() {
-    let version = xx::git::get_git_version();
-    println!("{}", version.unwrap());
+    //let version = xx::git::get_git_version();
+    //println!("{}", version.unwrap());
     let git = xx::git::Git::new(".".into());
 
     if git.is_repo() {
-        println!("{}", git.is_repo());
-        println!("{:}", format!("{}",git.get_remote_url().unwrap().to_string()));
+        //println!("{}", git.is_repo());
+        println!(
+            "{:}",
+            format!("{}", git.get_remote_url().unwrap().to_string())
+        );
+        println!("{:}", format!("{}", git.current_sha().unwrap().to_string()));
+        println!(
+            "{:}",
+            format!("{}", git.current_sha_short().unwrap().to_string())
+        );
+        println!(
+            "{:}",
+            format!("{}", git.current_branch().unwrap().to_string())
+        );
     }
 }
